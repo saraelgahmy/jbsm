1c2a795d2f54ca2ce09a5fc8d0e991e2bca32af6
saraelgahmy <127721746+saraelgahmy@users.noreply.github.com>
2023-04-06 12:16:06 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -871,6 +871,7 @@
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
+              
               <div class="text-center"><button type="submit">ارسال</button></div>
             </form>
           </div><!-- End Contact Form -->
